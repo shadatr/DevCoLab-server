c4968caaf4e5c4d8c214e819866f4565f96713bc
shadatr <shadadaab@gmail.com>
2023-11-19 17:51:03 +0300

    single post added

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,7 @@ const postSchema = new Schema({
   _user: {type: Schema.Types.ObjectId, ref:"user"},
   user_name: String,
   text:String,
-  images:[String] 
+  images:[Buffer] 
 });
 
 mongoose.model('posts', postSchema);
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,35 +1,42 @@
 const mongoose = require("mongoose");
-const { Binary } = require("mongodb");
 const multer = require("multer");
-const upload = multer();
-
 const Posts = mongoose.model("posts");
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
 
 module.exports = (app) => {
   app.post("/api/post", upload.array("images"), async (req, res) => {
-    try {
-      const post = new Posts({
-        _user: req.user.id,
-        user_name: req.user.name,
-        text: req.body.text,
-        images:req.body.images
-      });
+    const imagesArray = req.files.map((file) => file.buffer);
+    const post = new Posts({
+      _user: req.user.id,
+      user_name: req.user.name,
+      text: req.body.text,
+      images: imagesArray,
+    });
 
-
-    console.log(post);
-
-      await post.save();
-
-      res.status(201).send("Post saved successfully");
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
+    await post.save();
+    res.status(201).send("Post saved successfully");
   });
 
   app.get("/api/posts", async (req, res) => {
     const allPosts = await Posts.find();
-    // console.log(allPosts);
     res.send(allPosts);
-  })
+  });
+
+  app.get("/api/post/:id", async (req, res) => {
+    const postId = req.params.id;
+    try {
+      const post = await Posts.findById(postId);
+  
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+  
+      res.json(post);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
 };
